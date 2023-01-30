6bbb57f35d92d446adea62841bd2e9e24d9dbecd
Wan Sim <0420syj@naver.com>
2023-01-30 13:59:46 +0900

    Test : UserSession 테스트
    UserSession의 Fake 객체 생성하여 테스트

--- src/test/java/com/example/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/com/example/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,53 @@
 package com.example.tripservicekata.trip;
 
+import com.example.tripservicekata.exception.CollaboratorCallException;
+import com.example.tripservicekata.exception.UserNotLoggedInException;
+import com.example.tripservicekata.user.User;
+import com.example.tripservicekata.user.UserSession;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
 public class TripServiceTest {
-	
+
+    // Fake
+    public static class NullUserSession extends UserSession {
+        @Override
+        public User getLoggedUser() {
+            return null;
+        }
+    }
+
+    // Fake
+    public static class FakeExceptionUserSession extends UserSession {
+        @Override
+        public User getLoggedUser() {
+            return super.getLoggedUser();
+        }
+    }
+
+    @Test
+    void UserSession이_Null인_경우() {
+
+        UserSession userSession = new NullUserSession();
+        Trip trip = new Trip();
+        TripService tripService = new TripService(userSession);
+        User user = new User();
+
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+    }
+
+    @Test
+    void UserSession이_Null이_아닌_경우() {
+
+        UserSession userSession = new FakeExceptionUserSession();
+        Trip trip = new Trip();
+        TripService tripService = new TripService(userSession);
+        User user = new User();
+
+        assertThrows(CollaboratorCallException.class, () -> tripService.getTripsByUser(user));
+    }
+
 }
